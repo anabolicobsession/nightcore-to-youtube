bf3ac7e88913b8537bbab134d6d7c59f06438762
anabolicobsession <pilvorm@gmail.com>
2025-02-20 02:32:28 +0100

    Style(param-types): Adjust `RangeParamType` definition with `RatioParamType`

--- src/param_types.py
+++ src/param_types.py
@@ -9,18 +9,32 @@ class RangeParamType(click.ParamType):
         self.min_start = min_start
         self.max_end = max_end
 
-    def convert(self, value, param, ctx) -> TYPE:
+    def convert(self, value, param, ctx):
         try:
             start, end = map(int, value.split(':'))
-            if not (start <= end): raise ValueError('Start should be less or equal to end')
-            if not (self._is_within_range(start) and self._is_within_range(end)): raise ValueError(f'Given range is not within allowed range: `{self.min_start}:{self.max_end}`')
+
+            if not (start <= end):
+                raise ValueError('Start should be less or equal to end')
+
+            if not (self._is_within_range(start) and self._is_within_range(end)):
+                raise ValueError(f'Given range is not within allowed range: `{self.min_start}:{self.max_end}`')
+
             return start, end
 
         except Exception as e:
-            self.fail(f'Invalid range format: `{value}`. Expected format: `start:end`. Error: {str(e)}', param, ctx)
+            self.fail(
+                f'Invalid format: `{value}`. '
+                f'Expected format: `start:end`. '
+                f'Error: {str(e)}',
+                param,
+                ctx,
+            )
 
     def _is_within_range(self, x: int):
-        return self.min_start <= x <= self.max_end
+        return (
+            (not self.min_start or x >= self.min_start) and
+            (not self.max_end or x <= self.max_end)
+        )
 
 
 class RatioParamType(click.ParamType):
