318571337243c24a4ce96278a08bdf21ac08dab3
anabolicobsession <pilvorm@gmail.com>
2025-03-03 05:25:20 +0100

    Feat(upload-to-youtube): Add video sorting based on speed and generating speed names

--- src/upload_to_youtube.py
+++ src/upload_to_youtube.py
@@ -12,11 +12,6 @@ from src.working_directory import WorkingDirectory
 logger = logging.getLogger(__name__)
 
 
-def upload_to_youtube(working_directory: WorkingDirectory):
-    service = build('youtube', 'v3', credentials=get_credentials())
-    videos = working_directory.get_video_paths(raise_if_not_exist=True)
-    speeds = [working_directory.path_to_speed_and_reverb(x)[0] for x in videos]
-
 def get_credentials():
     creds = None
 
@@ -38,3 +33,47 @@ def get_credentials():
             pickle.dump(creds, file)
 
     return creds
+
+
+SLOWED_NAMES = [
+    'Deeply Slowed',
+    'Slowed',
+    'Slightly Slowed',
+]
+
+SPED_UP_NAMES = [
+    'Slightly Sped Up',
+    'Sped Up',
+    'Super Sped Up',
+]
+
+def generate_speed_names(amount_slowed, amount_sped_up):
+    match amount_slowed:
+        case 0: slowed = []
+        case 1: slowed = [1]
+        case 2: slowed = [0, 1]
+        case 3: slowed = [0, 1, 2]
+        case _: raise ValueError(f'Unexpected amount of slowed versions: {amount_slowed}')
+
+    match amount_sped_up:
+        case 0: sped_up = []
+        case 1: sped_up = [1]
+        case 2: sped_up = [1, 2]
+        case 3: sped_up = [0, 1, 2]
+        case _: raise ValueError(f'Unexpected amount of sped up versions: {amount_slowed}')
+
+    return [SLOWED_NAMES[x] for x in slowed] + [SPED_UP_NAMES[x] for x in sped_up]
+
+
+def upload_to_youtube(working_directory: WorkingDirectory):
+    service = build('youtube', 'v3', credentials=get_credentials())
+
+    # sort videos by speed
+    videos = working_directory.get_video_paths(raise_if_not_exist=True)
+    speeds = [working_directory.path_to_speed_and_reverb(x)[0] for x in videos]
+
+    amount_slowed = len([x for x in sorted(speeds) if x < config.STANDARD_SPEED])
+    sorted_speed_names = generate_speed_names(amount_slowed=amount_slowed, amount_sped_up=len(speeds) - amount_slowed)
+
+    sorted_videos_and_speeds = sorted(zip(videos, speeds), key=lambda x: x[1])
+    sorted_videos = list(zip(*sorted_videos_and_speeds))[0]
